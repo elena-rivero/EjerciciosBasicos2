2ac0454e6640e04c47043dcb848cad3aee4263d5
Elena <Elena@106-01.info2>
2022-09-28 13:50:01 +0200

    Ejercicio 3 resuelto

--- EjerciciosBasicos2/src/ejercicios/Ejercicio3.java
+++ EjerciciosBasicos2/src/ejercicios/Ejercicio3.java
@@ -1,9 +1,25 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Variable entera donde se guardará el número introducido por el usuario
+		int numero;
+		String res;
+		
+		// Creo el scanner para la lectura por teclado
+		Scanner sc = new Scanner(System.in);
+		
+		// Le pido al usuario un número
+		System.out.println("Introduzca un número: ");
+		// Leemos el número por teclado
+		numero = sc.nextInt();
+		
+		res = (numero%2==0) ? "es par" : "es impar";
+		System.out.println("El número " + res);
+		sc.close();
 
 	}
 
