f55742d5871f7ab2f93bb1a58a10e21199153d09
Elena <Elena@106-01.info2>
2022-09-29 13:07:07 +0200

    Solución Ejercicio 5 completa

--- EjerciciosBasicos2/src/ejercicios/Ejercicio5.java
+++ EjerciciosBasicos2/src/ejercicios/Ejercicio5.java
@@ -1,11 +1,30 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		// Variables para almacenar las notas de los tres trimestres
 		int notasPrimerT, notasSegundoT, notasTercerT;
+		double notaMedia;
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca la nota del Primer Trimestre");
+		notasPrimerT = sc.nextInt();
+
+		System.out.println("Introduzca la nota del Segundo Trimestre");
+		notasSegundoT = sc.nextInt();
+
+		System.out.println("Introduzca la nota del Tercer Trimestre");
+		notasTercerT = sc.nextInt();
+
+		notaMedia = (double) (notasPrimerT + notasSegundoT + notasTercerT) / 3;
+
+		System.out.println("La nota del boletín es: " + (int) notaMedia);
+		System.out.printf("La nota del expediente es: %.2f", notaMedia);
+		// cerramos Scanner
+		sc.close();
 	}
 
 }
