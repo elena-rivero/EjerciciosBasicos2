c15d5c003ff4cc3667ddc9035e1240067fc341ca
Elena <Elena@106-01.info2>
2022-09-29 12:47:54 +0200

    Solución del Ejercicio5

---
+++ EjerciciosBasicos2/src/ejercicios/Ejercicio5.java
@@ -0,0 +1,11 @@
+package ejercicios;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		// Variables para almacenar las notas de los tres trimestres
+		int notasPrimerT, notasSegundoT, notasTercerT;
+
+	}
+
+}
